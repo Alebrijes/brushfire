47781c00b12cc8a59eb073ec0dda05b3370f41fa
Alfredo Leyzaola <alfredoleyzaola@gmail.com>
2015-08-05 18:33:28 +0100

    chapter5

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -13,5 +13,53 @@ module.exports.bootstrap = function(cb) {
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+
+  Video.count().exec(function(err, numVideos) {
+
+    if (err) {
+      return cb(err);
+    }
+
+    if (numVideos > 0) {
+      console.log('Existing video records: ', numVideos)
+      return cb();
+    }
+
+    var Youtube = require('machinepack-youtube');
+                   //#A
+    // List Youtube videos which match the specified search query.
+    Youtube.searchVideos({
+      query: 'grumpy cat',
+      apiKey: 'AIzaSyBG6SfeuVny-lK22UvYJMXorKNxm3UXqYE',
+      limit: 15,
+    }).exec({
+      // An unexpected error occurred.
+      error: function(err) {
+      	return cb(err);
+      },
+      // OK.
+      success: function(returnedVideos) {
+
+      	 _.each(returnedVideos, function(video) {                   
+          video.src = 'https://www.youtube.com/embed/' + video.id; 
+          delete video.description;                               
+          delete video.publishedAt;                               
+          delete video.id;                                        
+          delete video.url;                                       
+        });
+
+      	 Video.create (returnedVideos).exec(function(err, videoRecordsCreated) {
+      	 	if (err) {
+      	 		return cb();
+      	 	}
+
+      	 	console.log(videoRecordsCreated);                              
+            return cb();
+      	 });
+      }, 
+   });
+    //return cb();
+  });
+
+  //cb();
 };
